cc253d125c5b1b0621b233b88aab0d89a62154a3
micchi320 <micchi320@gmail.com>
2016-02-20 18:14:02 +0000

    commit4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,15 +19,25 @@ class UsersController < ApplicationController
   end
   
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:seccess] = "Successfully updated"
+    # 保存に成功した場合はプロフィールへリダイレクト
+      redirect_to @user
+    else
+      flash[:alert] = "Updating Failed"
+      render 'edit'
+    end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,:password_confirmation)
+    params.require(:user).permit(:name, :email, :password,:password_confirmation,:age,:area)
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
     has_secure_password
+    validates :area, presence: true, length: { maximum: 50 }
+    validates :age , numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
